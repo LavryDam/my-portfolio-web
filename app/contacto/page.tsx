d50e93a5c88cb27daa8a0040ecd13330f8c77dda
Dina <98164900+LavryDam@users.noreply.github.com>
2024-11-20 12:34:41 +0100

    fix:contact-page
    logic and css

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,9 +1,29 @@
+import "./contactPage.css"
+
 export const metadata = {
     title: "Contacto",
 }
 
 function contactoPage(){
-    return <h1>Contacto</h1>
+    return(
+        <div className="form-div">
+            <h1>Déjame un mensaje</h1>
+            <form className="form">
+                <input type="text" placeholder="Nombre" required />
+                <input type="email" placeholder="Email" required />
+                <input 
+                    type="tel" 
+                    placeholder="Teléfono" 
+                    pattern="^\+?[0-9]{1,10}$" 
+                    maxLength={10} 
+                    title="Debe ser un número de 10 dígitos" 
+                    required
+                />
+                <textarea rows={5} placeholder="Si tienes algo que decirme..."></textarea>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
 }
 
 export default contactoPage;
