7be9919612eff4c6608aa405cf4cb7d3aa1b902b
Dina <98164900+LavryDam@users.noreply.github.com>
2024-11-21 11:50:48 +0100

    fix:img and text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,14 +11,13 @@ function aboutPage(){
         <>
             <div className="contenedor-about">
                 <div className="img-dina">
-                    <Image src="/docs/img-dina.jpg" width={250} height={250} alt="Imagen Dina"/>
+                    <Image src="/docs/img-dina.jpg" width={150} height={150} alt="Imagen Dina"/>
                 </div>
                 <div className="about-page">
                     <h1>Mi historia</h1>
                     <p className="bio-texto">
-                        Soy Dina Martinez Ruiz, desarrolladora de software apasionada y creativa con formación en Desarrollo de Aplicaciones Multiplataforma (DAM). Durante mis prácticas en una empresa en Ámsterdam, trabajé con React y React Native para desarrollar una aplicación multiplataforma, optimizada tanto para Android como para iOS, enfocada en ofrecer una experiencia de usuario intuitiva y de alto rendimiento.
-                        Actualmente, estoy en constante aprendizaje, explorando nuevas tecnologías como Next.js para mantenerme a la vanguardia y enfrentar nuevos retos en el desarrollo web y móvil.  
-                        Fuera del ámbito profesional, me apasiona la lectura, especialmente de novela negra, ciencia ficción y fantasía. También disfruto de la playa, donde encuentro inspiración y tranquilidad para recargar energías. Mi enfoque siempre está en crecer, aprender y aportar valor en cada proyecto que emprendo.
+                        Soy Dina Martinez Ruiz, desarrolladora de software apasionada y creativa con formación en Desarrollo de Aplicaciones Multiplataforma (DAM). Durante mis prácticas en una empresa en Ámsterdam, trabajé con React y React Native para desarrollar una aplicación multiplataforma, 
+                        optimizada tanto para Android como para iOS. Actualmente, estoy en constante aprendizaje, explorando nuevas tecnologías como Next.js para mantenerme a la vanguardia y enfrentar nuevos retos en el desarrollo web y móvil.  
                     </p>
                     <Link href="docs/CV DINA MARTINEZ RUIZ.pdf" target="_blank" download className="enlace-cv">Descargar Currículum Vitae</Link>
                 </div>
