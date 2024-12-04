b97e2fc24b481cdcb0b123ce6d5bb9556c7cc965
Dina <98164900+LavryDam@users.noreply.github.com>
2024-12-04 12:38:27 +0100

    fix:logic img projects

--- app/proyectos/page.tsx
+++ app/proyectos/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import "./projectPage.css";
+import Image from 'next/image'
 
 export const metadata = {
-    title: "Mis proyectos",
+    title: "Mis proyectos", 
 };
 
 function ProyectosPage() {
@@ -12,6 +13,12 @@ function ProyectosPage() {
             <div className="contenedor-proyectos">
                 <div className="tarjeta-proyecto">
                     <h3 className="titulo-proyecto">App de Recetas Saludables y Personalizadas</h3>
+                    <Image
+                        src="/docs/libroRecetas.jpg"
+                        width={310}
+                        height={200}
+                        alt="Picture of the author"
+                    />
                     <p className="detalle-proyecto">
                         Una app enfocada en recetas saludables que se adaptan a los objetivos de salud de los usuarios, como perder peso, ganar músculo o mantener una dieta equilibrada.
                     </p>
@@ -30,7 +37,13 @@ function ProyectosPage() {
                     </Link>
                 </div>
                 <div className="tarjeta-proyecto">
-                    <h3 className="titulo-proyecto">Rastreador de Gastos</h3>
+                    <h3 className="titulo-proyecto">App Rastreador de Gastos</h3>
+                    <Image
+                        src="/docs/libroRecetas.jpg"
+                        width={310}
+                        height={200}
+                        alt="Picture of the author"
+                    />
                     <p className="detalle-proyecto">
                         Una herramienta útil para gestionar tus finanzas personales, llevar un registro de tus ingresos y gastos, y planificar mejor tu presupuesto.
                         
