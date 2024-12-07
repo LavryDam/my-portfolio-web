f2f2206e0cdda125718f0a66382fe983922ca533
Dina <98164900+LavryDam@users.noreply.github.com>
2024-12-07 21:05:55 +0100

    fix:img
    css and logic

--- app/proyectos/project.tsx
+++ app/proyectos/project.tsx
@@ -11,31 +11,31 @@ function ProyectosPage() {
         <section className="proyectos" id="proyectos">
             <h2>Mis Proyectos</h2>
             <div className="contenedor-proyectos">
-                <div className="tarjeta-proyecto">
-                    <h3 className="titulo-proyecto">App de Recetas Saludables y Personalizadas</h3>
-                    <Image
-                        src="/images/libroRecetas.jpg"
-                        width={310}
-                        height={200}
-                        alt="Picture of the author"
-                    />
-                    <p className="detalle-proyecto">
-                        Una app enfocada en recetas saludables que se adaptan a los objetivos de salud de los usuarios, como perder peso, ganar músculo o mantener una dieta equilibrada.
-                    </p>
-                    <ul className="tecnologias">
-                        <li>Kotlin</li>
-                        <li>Firebase</li>
-                        <li>Android</li>
-                        <li>Retrofit</li>
-                    </ul>
-                    <Link
-                        href="https://github.com/LavryDam/portfolio-recipes-project.git"
-                        target="_blank"
-                        className="enlace-proyecto"
-                    >
-                        Ver en GitHub
-                    </Link>
-                </div>
+                    <div className="plantilla-movil">
+                        <h3 className="titulo-proyecto">App de Recetas Saludables y Personalizadas</h3>
+                        <Image
+                            src="/images/libroRecetas.jpg"
+                            width={310}
+                            height={200}
+                            alt="Picture of the author"
+                        />
+                        <p className="detalle-proyecto">
+                            Una app enfocada en recetas saludables que se adaptan a los objetivos de salud de los usuarios, como perder peso, ganar músculo o mantener una dieta equilibrada.
+                        </p>
+                        <ul className="tecnologias">
+                            <li>Kotlin</li>
+                            <li>Firebase</li>
+                            <li>Android</li>
+                            <li>Retrofit</li>
+                        </ul>
+                        <Link
+                            href="https://github.com/LavryDam/portfolio-recipes-project.git"
+                            target="_blank"
+                            className="enlace-proyecto"
+                        >
+                            Ver en GitHub
+                        </Link>
+                    </div>
                 <div className="tarjeta-proyecto">
                     <h3 className="titulo-proyecto">App Rastreador de Gastos</h3>
                     <Image
