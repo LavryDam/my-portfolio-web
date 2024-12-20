119ee2aac5ae252fe7e0210d65ee61a9152b3673
Dina <98164900+LavryDam@users.noreply.github.com>
2024-12-20 11:22:15 +0100

    fix:comentarios

--- app/components/Menu/HamburgerMenu.tsx
+++ app/components/Menu/HamburgerMenu.tsx
@@ -5,7 +5,7 @@ import "./HamburgerMenu.css";
 import Link from "next/link";
 
 const HamburgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false); // Controla si el menú está abierto
+  const [isOpen, setIsOpen] = useState(false); // Controla si el menú despegable está abierto
   const menuRef = useRef<HTMLDivElement>(null); //useRef crea una referencia al contenedor del menú (menuRef), 
   //que apunta al elemento HTML correspondiente.
 
@@ -15,23 +15,22 @@ const HamburgerMenu = () => {
 
   // Cierra el menú al hacer clic fuera
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
+    const handleClickOutside = (event: MouseEvent) => { //Función que se ejecuta cuando se hace clic fuera del menú
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) { //Si el clic no está dentro del menú desplegable (menuRef) 
+        setIsOpen(false); //Cierra el menú desplegable (isOpen = false)
       }
     };
     //evento global
     document.addEventListener("mousedown", handleClickOutside);
-    return () => { //Limpia el evento al desmontar el componente para evitar fugas de memoria
-      document.removeEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);//Elimina el evento de escucha cuando el componente se desmonta
     };
   }, []);
 
   return (
     <div className="hamburger-menu" ref={menuRef}>
-      {/* Botón Hamburguesa */}
       <button
-        className={`hamburger-button ${isOpen ? "open" : ""}`} //Agrega la clase "open" si el menú está abierto
+        className={`hamburger-button ${isOpen ? "open" : ""}`} //Agrega la clase "open" si el menú está abierto 
         onClick={toggleMenu}
         aria-label="Toggle menu" //Mejora la accesibilidad describiendo el propósito del botón.
       >
@@ -39,10 +38,8 @@ const HamburgerMenu = () => {
         <span className="line"></span>
         <span className="line"></span>
       </button>
-
-      {/* Menú Desplegable */}
-      <div className={`menu-content ${isOpen ? "open" : ""}`}>
-        <Link href="#inicio" className="menu-item" onClick={() => setIsOpen(false)}>
+      <div className={`menu-content ${isOpen ? "open" : ""}`}> 
+        <Link href="#inicio" className="menu-item" onClick={() => setIsOpen(false)}>{/*Cierra el menú despegable al hacer clic en un enlace*/}
           Inicio
         </Link>
         <Link href="#about" className="menu-item" onClick={() => setIsOpen(false)}>
