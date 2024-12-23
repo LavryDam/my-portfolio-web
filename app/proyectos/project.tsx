1856854bddde2779c17527d529f1538b8817ded1
Dina <98164900+LavryDam@users.noreply.github.com>
2024-12-23 10:50:00 +0100

    fix: css

--- app/proyectos/project.tsx
+++ app/proyectos/project.tsx
@@ -19,7 +19,7 @@ function ProyectosPage() {
                         alt="Picture of the author"
                     />
                     <h3 className="titulo-proyecto">App de Recetas Saludables y Personalizadas</h3>
-                    <p className="detalle-proyecto">
+                    <p className="detalle-proyecto-app">
                         Una app enfocada en recetas saludables que se adaptan a los objetivos de salud de los usuarios, como perder peso, ganar músculo o mantener una dieta equilibrada.
                     </p>
                     <ul className="tecnologias">
@@ -30,6 +30,33 @@ function ProyectosPage() {
                     </ul>
                     <Link
                         href="https://github.com/LavryDam/portfolio-recipes-project.git"
+                        target="_blank"
+                        className="enlace-proyecto"
+                    >
+                        Ver en GitHub
+                    </Link>
+                </div>
+                <div className="tarjeta-proyecto">
+                    <Image
+                        src="/images/libroRecetas.jpg"
+                        width={290}
+                        height={190}
+                        alt="Picture of the author"
+                    />
+                    <h3 className="titulo-proyecto">Portfolio web Dina Martinez Ruiz</h3>
+                    <p className="detalle-proyecto">
+                        Una web diseñada para destacar mis habilidades como programadora, 
+                        incluyendo proyectos como una app de recetas saludables y personalizadas, 
+                        desarrollada con tecnología Next.js.
+                    </p>
+                    <ul className="tecnologias">
+                        <li>Next.js</li>
+                        <li>Vercel</li>
+                        <li>TypeScript</li>
+                        <li>CSS</li>
+                    </ul>
+                    <Link
+                        href="https://github.com/LavryDam/my-portfolio-web.git"
                         target="_blank"
                         className="enlace-proyecto"
                     >
