233adbeb4ca26349fdabceffa48175991d946a9b
Dina <98164900+LavryDam@users.noreply.github.com>
2025-01-24 11:12:13 +0100

    fix: css

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({children}:RootLayoutProps) {
   return (
     <html lang="es">  
       <head>
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={Fonts.className}>
         <Navbar/>
