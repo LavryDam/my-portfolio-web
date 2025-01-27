271599a9e9e51a1894ae691970a0c778ef4073cd
Dina <98164900+LavryDam@users.noreply.github.com>
2025-01-27 12:57:45 +0100

    fix: new metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,15 @@ export default function RootLayout({children}:RootLayoutProps) {
         <meta name="author" content="Dina Martinez Ruiz" />
         <meta property="og:title" content="Dina Martinez Ruiz | Desarrolladora Web y Móvil" />
         <meta property="og:description" content="Descubre mi portafolio como desarrolladora web y móvil con proyectos destacados y habilidades técnicas." />
-        <meta property="og:image" content="/images/DinaAmsterdam.webp" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/9e04ec77-12f4-4d4c-8134-480534d428ec.png?token=hgl1W55LU9pNlbDUxBpUSm_G0MW6dWGoRRleElExKF0&height=625&width=1200&expires=33273978583" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://portfolio-web-dina-martinez.vercel.app/" />  
+        <meta property="og:url" content="https://portfolio-web-dina-martinez.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="portfolio-web-dina-martinez.vercel.app"/>
+        <meta property="twitter:url" content="https://portfolio-web-dina-martinez.vercel.app/"/>
+        <meta name="twitter:title" content="Dina Martinez Ruiz"/>
+        <meta name="twitter:description" content="Portfolio web Dina Martinez Ruiz"/>
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/9e04ec77-12f4-4d4c-8134-480534d428ec.png?token=hgl1W55LU9pNlbDUxBpUSm_G0MW6dWGoRRleElExKF0&height=625&width=1200&expires=33273978583"/>
       </head>
       <body className={Fonts.className}>
         <GoogleAnalytics />
