8e22b501bd22194b15d039b994676d507517dcf3
Dina <98164900+LavryDam@users.noreply.github.com>
2025-01-28 10:57:34 +0100

    fix:googleA

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -9,7 +9,7 @@ declare global {
 }
 import Script from "next/script";
 
-const GA_TRACKING_ID = "GTM-53ZW7QGK"; // Reemplaza con tu ID de seguimiento
+const GA_TRACKING_ID = "G-PNPEJXGWBK"; // Reemplaza con tu ID de seguimiento
 
 export default function GoogleAnalytics() {
   useEffect(() => {
