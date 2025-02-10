3902b521fcc8fd9207bb9bee6f4b2398864b5e27
Dina <98164900+LavryDam@users.noreply.github.com>
2025-02-10 10:52:42 +0100

    fix:404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+import "./notfound.css"; 
 export default function NotFound() {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Página no encontrada</p>
+        <div className="not-found-container">
+            <h1 className="not-found-title">404</h1>
+            <p className="not-found-message">Lo sentimos, la página que buscas no existe.</p>
+            <Link href="/">
+                <a className="not-found-link">Volver a Inicio</a>
+            </Link>
         </div>
     );
 }
