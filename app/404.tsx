2c873ddeec629c27c323277bb91d4c658085ddec
Dina <98164900+LavryDam@users.noreply.github.com>
2025-02-10 10:44:52 +0100

    fix:404

--- app/404.tsx
+++ app/404.tsx
@@ -1,16 +1,8 @@
-import Link from "next/link";
-import "./notfound.module.csscss";
-
 export default function NotFound() {
     return (
-        <section className="not-found">
-            <div className="not-found-content">
-                <h1>404</h1>
-                <p>Lo sentimos, la página que buscas no existe.</p>
-                <Link href="/">
-                    <a className="back-home">Volver a Inicio</a>
-                </Link>
-            </div>
-        </section>
+        <div>
+            <h1>404</h1>
+            <p>Página no encontrada</p>
+        </div>
     );
 }
